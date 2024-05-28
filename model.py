790ff301959bd8075ace41d1b5a0d63d913a8942
MatasSepikas <71885160+MatasSepikas@users.noreply.github.com>
2024-05-28 12:22:55 +0300

    Delete model.py

--- model.py
+++
@@ -1,9 +0,0 @@
-class MyBestModel:
-    def __init__(self):
-        pass
-
-    def predict(self, X):
-        return {"prediction": {
-            "class": "Fun",
-            "confidence": 0.9
-        }}
